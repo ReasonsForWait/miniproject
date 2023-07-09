71d71b62c2a530c9f12bc4545d53f147723b5ae2
ReasonsForWait <leekjgo21@naver.com>
2023-07-09 17:06:04 +0900

    siatPJ2 연습용

--- draganddrop/src/App.js
+++ draganddrop/src/App.js
@@ -1,8 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import Dessert from './Component/Dessert';
-import Drink from './Component/Drink';
-import Meal from './Component/Meal';
+import Food from './Component/Food';
 
 import banana from './images/banana.png'
 import beer from './images/beer.png'
@@ -31,47 +29,6 @@ function App() {
     console.log(img);
   }
 
-  function onDropDrink(e){
-    e.preventDefault();
-    const types = img.food;
-    const id = img.id;
-    if (types !== "drink") {
-      alert(`${types} is not drink`);
-      return;
-    }
-    setImg({...img, id : id});
-  }
-  
-  function onDropDessert(e){
-    e.preventDefault();
-    const types = img.food;
-    const id = img.id;
-    if (types !== "dessert") {
-      alert(`${types} is not dessert`);
-      return;
-    }
-    setImg({...img, id : id});
-  }
-  
-  function onDropMeal(e){
-    e.preventDefault();
-    const types = img.food;
-    const id = img.id;
-    if (types !== "meal") {
-      alert(`${types} is not meal`);
-      return;
-    }
-    setImg({...img, id : id});
-  }
-
-  function onDragOver(e){
-    e.preventDefault();
-  }
-
-  function printAnyway(){
-    console.log("찍히나?");
-  }
-
   const imgSrc = [banana, beer, bread, cherry, coffee, juice, noodle, orange, rice];
   const foodDataset = { banana : "dessert", beer : "drink", bread : "meal", cherry : "dessert",
                        coffee : "drink", juice : "drink", noodle : "meal", orange : "dessert", rice : "meal"}
@@ -85,19 +42,18 @@ function App() {
           <img id={`f${i+1}`} src={imgSrc[i]} alt="음식" data-food={foodDataset[imgSrc[i]]}/>
         </li>
       );
-     }
+    }
      
-     return (
-       // ul 태그
-       <ul id='food' onDragStart={onDragStart}>{tags}</ul>
-     );
+    return (
+      <ul id='food' onDragStart={onDragStart}>{tags}</ul>
+    );
   }
 
   return (
     <div>
-      <h2>준비된 음식 종류</h2>
-      {renderFood()}
-      {/* <ul id='food' onDragStart={onDragStart}>
+      <h2>준비된 음식</h2>
+      {/* {renderFood()} */}
+      <ul>
         <li><img id="f1" src={banana} alt="바나나" data-food="dessert"></img></li>
         <li><img id="f2" src={beer} alt="맥주" data-food="drink"></img></li>
         <li><img id="f3" src={bread} alt="빵" data-food="meal"></img></li>
@@ -107,11 +63,10 @@ function App() {
         <li><img id="f7" src={noodle} alt="면" data-food="meal"></img></li>
         <li><img id="f8" src={orange} alt="오렌지" data-food="dessert"></img></li>
         <li><img id="f9" src={rice} alt="쌀밥" data-food="meal"></img></li>
-      </ul> */}
-
-      <Dessert onDragOver={onDragOver} onDrop={printAnyway}></Dessert>
-      <Drink></Drink>
-      <Meal></Meal>
+      </ul>
+      <Food category='dessert'></Food>
+      <Food category='drink'></Food>
+      <Food category='meal'></Food>
     </div>
   );
 }
